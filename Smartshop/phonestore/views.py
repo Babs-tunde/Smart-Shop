9c9191505a54283755a3ec225391fd1e2f27872d
Babs-tunde <112407303+Babs-tunde@users.noreply.github.com>
2023-01-18 15:41:29 -0800

    Filter and styling
    created filter in the invoice list

--- Smartshop/phonestore/views.py
+++ Smartshop/phonestore/views.py
@@ -12,48 +12,53 @@ def phone_list(request):
     phones = Phone.objects.all()
     return render(request, 'phone_list.html', {'phones': phones})
 
-# def create_invoice(request):
-#     if request.method == 'POST':
-#         phone = Phone.objects.get(id=request.POST['phone'])
-#         phone.stock -= int(request.POST['quantity'])
-#         phone.save()
-
-#         invoice = Invoice(customer_name=request.POST['customer_name'],
-#                           customer_address=request.POST['customer_address'],
-#                           date=request.POST['date'],
-#                           phone=phone,
-#                           quantity=request.POST['quantity'])
-#         invoice.save()
-#         return redirect('invoice_list')
-#     else:
-#         phones = Phone.objects.all()
-#         return render(request, 'create_invoice.html', {'phones': phones})
-
-
 def create_invoice(request):
     if request.method == 'POST':
-        customer_name = request.POST['customer_name']
-        customer_address = request.POST['customer_address']
-        date = request.POST['date']
-        total_amount = 0
-        for i in range(len(request.POST.getlist('phone'))):
-            phone = Phone.objects.get(pk=request.POST.getlist('phone')[i])
-            quantity = request.POST.getlist('quantity')[i]
-            total_amount += phone.price * int(quantity)
-            invoice = Invoice.objects.create(customer_name=customer_name, customer_address=customer_address, date=date, phone=phone, quantity=quantity)
-        invoice.total_amount = total_amount
+        phone = Phone.objects.get(id=request.POST['phone'])
+        phone.stock -= int(request.POST['quantity'])
+        phone.save()
+
+        invoice = Invoice(customer_name=request.POST['customer_name'],
+                          customer_address=request.POST['customer_address'],
+                          date=request.POST['date'],
+                          phone=phone,
+                          quantity=request.POST['quantity'])
         invoice.save()
         return redirect('invoice_list')
     else:
         phones = Phone.objects.all()
         return render(request, 'create_invoice.html', {'phones': phones})
 
+
+# def create_invoice(request):
+#     if request.method == 'POST':
+#         customer_name = request.POST['customer_name']
+#         customer_address = request.POST['customer_address']
+#         date = request.POST['date']
+#         total_amount = 0
+#         for i in range(len(request.POST.getlist('phone'))):
+#             phone = Phone.objects.get(pk=request.POST.getlist('phone')[i])
+#             quantity = request.POST.getlist('quantity')[i]
+#             total_amount += phone.price * int(quantity)
+#             invoice = Invoice.objects.create(customer_name=customer_name, customer_address=customer_address, date=date, phone=phone, quantity=quantity)
+#         invoice.total_amount = total_amount
+#         invoice.save()
+#         return redirect('invoice_list')
+#     else:
+#         phones = Phone.objects.all()    
+#         return render(request, 'create_invoice.html', {'phones': phones})
+
+
 # def invoice_list(request):
 #     invoices = Invoice.objects.all()
 #     return render(request, 'invoice_list.html', {'invoices': invoices})
 
 def invoice_list(request):
-    invoices = Invoice.objects.all()
+    if request.method == 'POST':
+        date = request.POST["date"]
+        invoices = Invoice.objects.filter(date=date)
+    else:
+        invoices = Invoice.objects.all()
     for invoice in invoices:
         invoice.price = invoice.quantity * invoice.phone.price
     return render(request, 'invoice_list.html', {'invoices': invoices})
